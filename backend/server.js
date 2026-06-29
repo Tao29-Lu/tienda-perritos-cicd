bca4cd626067d4c4bc47177573bda8a09cd5f6fa
Lucero2024 <luce.gutierrez@duocuc.cl>
2026-06-29 02:46:18 -0400

    Adaptar proyecto para despliegue en EKS

--- backend/server.js
+++ backend/server.js
@@ -6,9 +6,9 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 const {
-  DB_HOST = "10.0.3.209", // acá colocar la IP Privada EC2 DB
-  DB_USER = "root",
-  DB_PASSWORD = "admin123",
+  DB_HOST = "mysql-service",
+  DB_USER = "tienda_user",
+  DB_PASSWORD = "tienda123",
   DB_NAME = "tienda_perritos",
   DB_PORT = 3306,
 } = process.env;
